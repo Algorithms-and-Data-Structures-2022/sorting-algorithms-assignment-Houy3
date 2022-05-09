d9d5925e5fd1643c21ce03bb7b2afd6ef0638d95
Houy3 <ockap200304008@gmail.com>
2022-05-09 14:21:23 +0300

    hj

--- src/merge_sort.cpp
+++ src/merge_sort.cpp
@@ -6,25 +6,19 @@
 namespace assignment {
 
   void MergeSort::Sort(std::vector<int>& arr) const {
-
-    // буфер памяти для операции слияния (merge)
     std::vector<int> buf(arr.size());
-
-    // забыл что-то здесь вызвать ...
+    merge_sort(arr, 0, arr.size()-1, buf);
   }
 
   void MergeSort::merge_sort(std::vector<int>& arr, int start, int stop, std::vector<int>& buf) const {
-
-    // выход из рекурсии: подмассив длины один
     if (start >= stop) {
       return;
     }
 
-    // вычисляем индекс середины области
     const int middle = middle_of(start, stop);
-
-    // рекурсивный вызов сортировки левой [start, middle] и правой [middle + 1, stop] подмассивов ...
-    // слияния двух подмассивов [start, middle] и [middle + 1, stop] ...
+    merge_sort(arr,start, middle, buf);
+    merge_sort(arr, middle+1, stop, buf);
+    merge(arr, start, middle, stop, buf);
   }
 
 }  // namespace assignment
--- src/quick_sort.cpp
+++ src/quick_sort.cpp
@@ -17,11 +17,9 @@ namespace assignment {
       return;
     }
 
-    // вычисляем индекс опорного элемента ... median_of_three ...
-    int pivot_index = -1 /* напишите здесь свой код ... */;
-
-    // производим разбиение относительно опорного элемента ...  partition ...
-    // рекурсивно повторяем процедуру над левой и правой частью ...
+    int pivot_index = partition(arr, start, stop, median_of_three(arr,start,stop));
+    quick_sort(arr, start, pivot_index-1);
+    quick_sort(arr, pivot_index+1,stop);
   }
 
 }  // namespace assignment
